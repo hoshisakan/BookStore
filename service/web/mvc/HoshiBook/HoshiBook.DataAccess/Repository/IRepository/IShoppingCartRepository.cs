f74b03c2c46bcce4dd91310e22457650683059c4
YiQuan Zhao <hoshisakan@gmail.com>
2023-01-27 22:30:25 +0800

    2023-01-27-second-commit-add-shopping-cart-and-application-user-repository-and-add-new-items-to-shopping-cart-and-add-items-to-existing-cart-completed

---
+++ service/web/mvc/HoshiBook/HoshiBook.DataAccess/Repository/IRepository/IShoppingCartRepository.cs
@@ -0,0 +1,17 @@
+using HoshiBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace HoshiBook.DataAccess.Repository.IRepository
+{
+    public interface IShoppingCartRepository : IRepository<ShoppingCart>
+    {
+        int IncrementCount(ShoppingCart shoppingCart, int count);
+        int DecrementCount(ShoppingCart shoppingCart, int count);
+    }
+}
