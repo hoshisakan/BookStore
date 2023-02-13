01b938cb568648da2de503715ad1d04f7972e931
YiQuan Zhao <hoshisakan@gmail.com>
2023-02-13 21:17:25 +0800

    2023-02-13-add-session-clear-for-remove-user-shopping-cart-product-total-count

--- service/web/mvc/HoshiBook/HoshiBookWeb/Areas/Customer/Controllers/CartController.cs
+++ service/web/mvc/HoshiBook/HoshiBookWeb/Areas/Customer/Controllers/CartController.cs
@@ -244,6 +244,7 @@ namespace HoshiBookWeb.Areas.Customer.Controllers
             List<ShoppingCart> shoppingCarts = _unitOfWork.ShoppingCart.GetAll(
                 u => u.ApplicationUserId == orderHeader.ApplicationUserId
             ).ToList();
+            HttpContext.Session.Clear();
             _unitOfWork.ShoppingCart.RemoveRange(shoppingCarts);
             _unitOfWork.Save();
             return View(id);
