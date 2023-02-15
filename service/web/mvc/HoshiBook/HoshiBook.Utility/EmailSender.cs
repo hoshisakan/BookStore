c9e5160a7e21258a6558c12c8902f1597b35de93
YiQuan Zhao <hoshisakan@gmail.com>
2023-02-15 22:50:42 +0800

    2023-02-15-first-commit-remove-SendGrid-about-code-and-modify-EmailSender-parameters-move-to-appsettings-json-file

--- service/web/mvc/HoshiBook/HoshiBook.Utility/EmailSender.cs
+++ service/web/mvc/HoshiBook/HoshiBook.Utility/EmailSender.cs
@@ -1,56 +1,59 @@
-// using System;
-// using System.Collections.Generic;
-// using System.Linq;
-// using System.Text;
-// using System.Threading.Tasks;
 using MailKit.Net.Smtp;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.Extensions.Configuration;
 using MimeKit;
-// using SendGrid;
-// using SendGrid.Helpers.Mail;
-using static HoshiBook.Utility.Config;
+
 
 namespace HoshiBook.Utility
 {
     public class EmailSender : IEmailSender
     {
-        // public string SendGridSecret { get; set; }
+        public Dictionary<string, object> SMTPGoogle { get; private set; }
+        public Dictionary<string, object> SMTPSenderGrid { get; private set; }
 
         public EmailSender(IConfiguration _config)
         {
-            // SendGridSecret = _config["SendGrid:SecretKey"];
-            // SendGridSecret = _config.GetValue<string>("SendGrid:SecretKey");
-            // Console.WriteLine($"SendGridSecret: {SendGridSecret}");
+            SMTPGoogle = new Dictionary<string, object>(){
+                {"Host", _config.GetValue<string>("SMTP:Google:Host")},
+                {"Port", _config.GetValue<int>("SMTP:Google:Port")},
+                {"Username", _config.GetValue<string>("SMTP:Google:Username")},
+                {"Password", _config.GetValue<string>("SMTP:Google:Password")},
+                {"Sender", _config.GetValue<string>("SMTP:Google:Sender")},
+            };
+            SMTPSenderGrid = new Dictionary<string, object>(){
+                {"Host", _config.GetValue<string>("SMTP:SenderGrid:Host")},
+                {"Port", _config.GetValue<int>("SMTP:SenderGrid:Port")},
+                {"Username", _config.GetValue<string>("SMTP:SenderGrid:Username")},
+                {"Password", _config.GetValue<string>("SMTP:SenderGrid:Password")}
+            };
         }
 
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
             //TODO Send email with MimeKit and MailKit (works)
             var emailToSend = new MimeMessage();
-            emailToSend.From.Add(MailboxAddress.Parse("hello@dotnetmastery.com"));
+            emailToSend.From.Add(MailboxAddress.Parse(SMTPGoogle["Sender"].ToString()));
             emailToSend.To.Add(MailboxAddress.Parse(email));
             emailToSend.Subject = subject;
             emailToSend.Body = new TextPart(MimeKit.Text.TextFormat.Html){
                 Text = htmlMessage
             };
-            //send email
+            //TODO Send email
             using (var emailClient = new SmtpClient())
             {
                 emailClient.Connect(
-                    "smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls
+                    SMTPGoogle["Host"].ToString(),
+                    Convert.ToInt32(SMTPGoogle["Port"]),
+                    MailKit.Security.SecureSocketOptions.StartTls
                 );
-                emailClient.Authenticate(GoolgeSenderMailName, GoolgeSenderMailPassword);
+                emailClient.Authenticate(
+                    SMTPGoogle["Username"].ToString(),
+                    SMTPGoogle["Password"].ToString()
+                );
                 emailClient.Send(emailToSend);
                 emailClient.Disconnect(true);
             }
             return Task.CompletedTask;
-            //TODO Not working cannot send email to recipient from SendGrid specified sender email
-            // var client = new SendGridClient(SendGridSecret);
-            // var from = new EmailAddress("todosender000@gmail.com", "Hoshi Book");
-            // var to = new EmailAddress("qunjend897436@gmail.com ");
-            // var msg = MailHelper.CreateSingleEmail(from, to , subject, "", htmlMessage);
-            // return client.SendEmailAsync(msg);
         }
     }
 }
