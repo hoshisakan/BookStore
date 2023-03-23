b82327f8ecd5be6f1bc8fe6f611330f13fc7f9be
YiQuan Zhao <hoshisakan@gmail.com>
2023-03-23 21:23:43 +0800

    2023-03-23-third-commit-remove-CoverTypeController-old-delete-method

--- service/web/HoshiBook/HoshiBookWeb/Areas/Admin/Controllers/CoverTypeController.cs
+++ service/web/HoshiBook/HoshiBookWeb/Areas/Admin/Controllers/CoverTypeController.cs
@@ -87,39 +87,6 @@ namespace HoshiBookWeb.Areas.Admin.Controllers
             return View(obj);
         }
 
-        //GET
-        public IActionResult Delete(int? id)
-        {
-            if (id == null || id == 0) {
-                return NotFound();
-            }
-
-            var CoverTypeFromDbFirst = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
-
-            if (CoverTypeFromDbFirst == null) {
-                return NotFound();
-            }
-            return View(CoverTypeFromDbFirst);
-        }
-
-        //POST
-        //TODO Add ValidateAntiForgeryToken to avoid CORS attack
-        [HttpPost,ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public IActionResult DeletePOST(int? id)
-        {
-            var obj = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
-
-            if (obj == null) {
-                return NotFound();
-            }
-            _unitOfWork.CoverType.Remove(obj);
-            _unitOfWork.Save();
-            TempData["success"] = "CoverType deleted successfully";
-
-            return RedirectToAction("Index");
-        }
-
         #region API CALLS
 
         [HttpGet]
